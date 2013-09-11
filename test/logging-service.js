4bb50d92218a7d7fff02a9111d9b57340784bfbe
Alfio Zappala <azappala@azaptree.com>
2013-09-11 19:18:19 -0400

    more unit tests

--- test/logging-service.js
+++ test/logging-service.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright [2013] [runrightfast.co]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 'use strict';
 
 var expect = require('chai').expect;
@@ -24,6 +39,24 @@ describe("LoggingService", function() {
 
 		expect(loggingService.invalidEventCount).to.equal(0);
 		expect(loggingService.eventCount).to.be.gt(0);
+	});
+
+	it('can accept null options', function() {
+		var loggingService = require('../lib')(null);
+		loggingService.log({
+			tags : [ 'info' ],
+			data : 'LoggingService can except null options'
+		});
+		expect(loggingService.invalidEventCount).to.equal(0);
+	});
+
+	it('can accept undefined options', function() {
+		var loggingService = require('../lib')(undefined);
+		loggingService.log({
+			tags : [ 'info' ],
+			data : 'LoggingService can accept undefined options'
+		});
+		expect(loggingService.invalidEventCount).to.equal(0);
 	});
 
 	it("can emit events to a registered log listener that was specified in the config options", function(done) {
